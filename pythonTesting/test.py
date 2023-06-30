565f5ac7a0ff4b47952665fce289125dcaef947e
George Tegos <george.tegos.22@gmail.com>
2023-06-30 13:15:15 +0300

    seperate function input for people and user

--- pythonTesting/test.py
+++ pythonTesting/test.py
@@ -25,19 +25,29 @@ class people:
         print(self.name, self.age, self.email, self.phone, self.password)
 
 class user(people):
-     pass
+    
+    def __init__(self, name, age, email, phone, password, userID):
+        super().__init__(name, age, email, phone, password)
+        self.userID = userID
+        pass
 
 class employee(people):
     pass
 
-userInfo = user("george",29,"gtultim@asd.com",123123,11111111)
+userInfo = user("george",29,"gtultim@asd.com",123123,11111111,19)
 
 employeeInfo = employee("nick",34,"ggg@sss.cas",1234234,222222)
 
-def newInput():
+def userInput():
+    userX = user(input('Name '),input('age '),input("email "), input("phone "), input("Password "), input("ID "))
+    print(userX.name, userX.age, userX.email, userX.phone, userX.password, userX.id)
+
+def peopleInfo():
     userX = people(input('Name '),input('age '),input("email "), input("phone "), input("Password "))
     print(userX.name, userX.age, userX.email, userX.phone, userX.password)
 
-newInput()
+
+userInput()
+peopleInfo()
 
 
