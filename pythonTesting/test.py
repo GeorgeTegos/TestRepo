02bb0ef7390ee9900e0b0e8e0e0d70698851b358
George Tegos <george.tegos.22@gmail.com>
2023-06-30 13:25:25 +0300

    fixed errors and create input option

--- pythonTesting/test.py
+++ pythonTesting/test.py
@@ -40,14 +40,22 @@ employeeInfo = employee("nick",34,"ggg@sss.cas",1234234,222222)
 
 def userInput():
     userX = user(input('Name '),input('age '),input("email "), input("phone "), input("Password "), input("ID "))
-    print(userX.name, userX.age, userX.email, userX.phone, userX.password, userX.id)
+    print(f"\nName: ",userX.name,"\nAge: ", userX.age, "\nEmail: ", userX.email,"\nPhone: ", userX.phone,"\nPassword: ", userX.password,"\nYour ID: ", userX.userID)
 
 def peopleInfo():
     userX = people(input('Name '),input('age '),input("email "), input("phone "), input("Password "))
-    print(userX.name, userX.age, userX.email, userX.phone, userX.password)
+    print(f"Name: ",userX.name,"\nAge: ", userX.age, "\nEmail: ", userX.email,"\nPhone: ", userX.phone,"\nPassword: ", userX.password)
 
+whoIsIt = input("Who are you ? \n1) New \n2) Already User \n3) Employee\n")
+whoIsIt = int(whoIsIt)
+if whoIsIt == 1:
+    peopleInfo()
+elif whoIsIt == 2:
+    userInput()
+elif whoIsIt == 3:
+    pass
+else:
+    print("Wrong input")
 
-userInput()
-peopleInfo()
 
 
